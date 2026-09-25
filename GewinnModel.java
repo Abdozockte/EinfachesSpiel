273d84b3670d720bf480b3fd45d26fb7c681afce
Abdelrahman Hussein <ahussein2@student.tgm.ac.at>
2026-09-25 11:11:51 +0200

    Fehler bei berechneRunde gefixxt

--- GewinnModel.java
+++ GewinnModel.java
@@ -24,7 +24,7 @@ public class GewinnModel{
         if(computerZahl == spielerZahl){
             rundenErgebnis += 20;
         }
-        if(spielerZahl ==(++computerZahl) || spielerZahl ==(--computerZahl)){
+        if(spielerZahl ==(computerZahl + 1) || spielerZahl ==(computerZahl - 1)){
             rundenErgebnis += 5;
         }
         else{
